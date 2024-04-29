857a7d110d2b7ef6e3c3b2ddea59ba5a01056489
benjimusmaximus <benjaminbician@gmail.com>
2024-04-29 20:07:12 +0200

    con_sec

--- src/rpsPage.php
+++ src/rpsPage.php
@@ -5,23 +5,27 @@ use Doctrine\DBAL\DriverManager;
 
 $connectionParams = [
     'dbname' => 'rps',
-    'user' => 'user',
-    'password' => 'secret',
+    'user' => 'root',
+    'password' => '',
     'host' => 'localhost',
     'driver' => 'pdo_mysql',
 ];
-
 $conn = DriverManager::getConnection($connectionParams);
 $queryBuilder = $conn->createQueryBuilder();
 
 $queryBuilder
-    ->select('p.player_id', 'p2.player_id',  'r.value_id',  'r2.value_id',  'p3.player_id')
-    ->from('game', 'g')
-    ->innerJoin('g', 'player', 'p', 'g.player1 = p.player_id')
-    ->innerJoin('g', 'player', 'p2', 'g.player2 = p.player_id')
-    ->innerJoin('g', 'rps_value', 'r', 'g.player1Value = r.value_id')
-    ->innerJoin('g', 'rps_value', 'r2', 'g.player2Value = r.value_id')
-    ->innerJoin('g', 'player', 'p3', 'g.winner = p.player_id');
+    ->select('g.game_id', 'player1', 'player2')
+    ->from('game', 'g');
 
-$query = $queryBuilder->getSQL();
-echo $query;
+$stmt = "";
+// Ausführen des SQL-Statements
+try {
+    $stmt = $conn->executeQuery($queryBuilder->getSQL());
+} catch (\Doctrine\DBAL\Exception $e) {
+}
+
+// Alle Ergebnisse abrufen
+while (($row = $stmt->fetchAssociative()) != false ) {
+    echo $row['player1'];
+    echo "\n";
+}
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit5736fd3e9a310caa6d2489c293a74f6c
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit5736fd3e9a310caa6d2489c293a74f6c::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
