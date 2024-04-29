c93fd9a0674ae1858cbd91550feeee99d25a037b
aswa <alexander.nems@gmail.com>
2024-04-30 00:46:32 +0200

    final version

--- src/insert_game.php
+++ src/insert_game.php
@@ -22,13 +22,65 @@ $winner = $_POST['winner'];
 $date = $_POST['date'];
 
 
-echo "Insert is ready";
+$qb = $conn->createQueryBuilder();
+
+
+$player1Id = $qb->select('player_id')
+    ->from('player')
+    ->where('name = :name')
+    ->setParameter('name', $player1)
+    ->executeQuery()->fetchOne();
+
+if ($player1Id == '') {
+    $conn->insert('player',
+        ['name' => $player1]);
+    $player1Id = $qb->select('player_id')
+        ->from('player')
+        ->where('name = :name')
+        ->setParameter('name', $player1)
+        ->executeQuery()->fetchOne();
+}
+
+$player2Id = $qb->select('player_id')
+    ->from('player')
+    ->where('name = :name')
+    ->setParameter('name', $player2)
+    ->executeQuery()->fetchOne();
+
+if ($player2Id == '') {
+    $conn->insert('player', ['name' => $player2]);
+    $player2Id = $qb->select('player_id')
+        ->from('player')
+        ->where('name = :name')
+        ->setParameter('name', $player2)
+        ->executeQuery()->fetchOne();
+}
+
+
+$valueAId = $qb->select('value_id')
+    ->from('rps_value', 'r')
+    ->where('r.name = :name')
+    ->setParameter('name', $valueA)
+    ->executeQuery()->fetchOne();
+
+$valueBId = $qb->select('value_id')
+    ->from('rps_value', 'r')
+    ->where('r.name = :name')
+    ->setParameter('name', $valueB)
+    ->executeQuery()->fetchOne();
+
+$winnerId = $qb->select('p.player_id')
+    ->from('player', 'p')
+    ->where('p.name = :name')
+    ->setParameter('name', $winner)
+    ->executeQuery()->fetchOne();
+
 $conn->insert('game', [
-    'player1' => $player1,
-    'player2' => $player2,
-    'player1Value' => $valueA,
-    'player2Value' => $valueB,
-    'winner' => $winner,
+    'player1' => $player1Id,
+    'player2' => $player2Id,
+    'player1Value' => $valueAId,
+    'player2Value' => $valueBId,
+    'winner' => $winnerId,
     'date' => $date
 ]);
 header('Location: rpsPage.php');
